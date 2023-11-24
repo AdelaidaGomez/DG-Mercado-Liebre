bd070401789e5311215ad1193a9d4afd410c26ad
AdelaidaGomez <agomezvieco@gmail.com>
2023-11-23 20:02:13 -0500

    Subo repo Mercado Liebre

---
+++ app.js
@@ -0,0 +1,28 @@
+// Requerimos express y lo ejecutamos para tener disponibles todos los metodos que vamos a precisar
+const express = require("express");
+const app = express();
+
+// Modulo nativo para manejar las rutas de los archivos
+const path = require("path");
+
+// Usando recursos estáticos.
+const publicFolderPath = path.resolve(__dirname, "./public")
+app.use(express.static(publicFolderPath));
+
+// Ponemos a escuchar el servidor
+app.listen(3035, () => {
+    console.log("Servidor corriendo en http://localhost:3035")
+});
+
+// Definimos las rutas a los distintos pedidos que nuestro sitio sabe responder
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/home.html"))
+})
+//register
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"))
+})
+//LogIn
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"))
+})
---
+++ materiales/colorPallet.md
@@ -0,0 +1,18 @@
+.amarillo{
+    color: #EAC926
+}
+.azul{
+    color: #03264C
+}
+.celeste{
+    color: #1259c3
+}
+.gris-oscuro{
+    color: #666
+}
+.gris-claro{
+    color: #ebebeb
+}
+.verde{
+    color: #39b54a
+} */
